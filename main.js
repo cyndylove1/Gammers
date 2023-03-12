0e59d33d6520115dbc06824e1dc5cb58a2303062
cynthia nweke <nwekecynthia2015@gmail.com>
2023-03-12 11:40:42 +0100

    work

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+ const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2000,
+    reset: true
+
+ })
+ sr.reveal('.main-content,.image-right',{delay:350, origin:'top'})
+
+ 
+ sr.reveal('.game-zone,.about,.section,.sections,.tournament,.contact,.footer,.developer', {delay:350, origin:'bottom'})
