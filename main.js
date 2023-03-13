52dc85766ce2c176c13c58cabe8bed50ff9316a6
cynthia nweke <nwekecynthia2015@gmail.com>
2023-03-13 18:39:12 +0100

    work

--- main.js
+++ main.js
@@ -12,4 +12,4 @@
  sr.reveal('.main-content,.image-right',{delay:350, origin:'top'})
 
  
- sr.reveal('.zone-content,.about-text,.about-img,.games-cards,.categories,.games-card,.categorie,.tournament1,.contact-content,.footer-text,.developer-section,.popular-content', {delay:350, origin:'bottom'})
+ sr.reveal('.zone-content,.about-text,.about-img,.games-cards,.categories,.games-card,.categorie,.tournament1,.contact-content,.footer-content,.links,.action,.getintouch,.developer-section,.popular-content,.copy', {delay:350, origin:'bottom'})
